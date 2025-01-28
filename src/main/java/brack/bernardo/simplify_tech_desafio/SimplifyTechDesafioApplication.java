e0a2b9cd557d0d17efa9ad955297588c8a6a7eff
Bernardo Brack <be.gbrack@gmail.com>
2025-01-28 14:37:54 -0300

    Add initial project config

---
+++ src/main/java/brack/bernardo/simplify_tech_desafio/SimplifyTechDesafioApplication.java
@@ -0,0 +1,13 @@
+package brack.bernardo.simplify_tech_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifyTechDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifyTechDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brack/bernardo/simplify_tech_desafio/SimplifyTechDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package brack.bernardo.simplify_tech_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifyTechDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
